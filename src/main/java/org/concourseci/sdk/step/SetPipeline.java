25775f23924c02458f5a6a96402fdbaf88a164bf
Kevin Bimonte <kbimonte@gmail.com>
2025-03-16 20:04:30 -0400

    feat: Add vars support to set pipeline

--- src/main/java/org/concourseci/sdk/step/SetPipeline.java
+++ src/main/java/org/concourseci/sdk/step/SetPipeline.java
@@ -1,8 +1,12 @@
 package org.concourseci.sdk.step;
 
+import com.google.gson.JsonObject;
 import com.google.gson.annotations.SerializedName;
 import org.concourseci.sdk.resource.Get;
 import org.concourseci.sdk.util.Validator;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class SetPipeline implements IStep {
 
@@ -13,11 +17,31 @@ public class SetPipeline implements IStep {
 
     private String team = null;
 
+    @SerializedName("var_files")
+    private Set<String> varFiles = null;
+
+    private JsonObject vars = null;
+
+    @SerializedName("instance_vars")
+    private JsonObject instanceVars = null;
+
     private SetPipeline(String name, String file) {
         Validator.validateIdentifier(name);
 
         this.name = name;
         this.file = file;
+    }
+
+    public static SetPipeline create(String name, String path) {
+        Validator.validateIdentifier(name);
+
+        if (path != null && path.startsWith("/")) {
+            path = path.trim().substring(1);
+        }
+
+        String file = String.format("%s", path);
+
+        return new SetPipeline(name, file);
     }
 
     public static SetPipeline create(String name, Get repo, String path) {
@@ -36,6 +60,52 @@ public class SetPipeline implements IStep {
         Validator.validateIdentifier(team);
 
         this.team = team;
+
+        return this;
+    }
+
+    public SetPipeline addVarFile(Get repo, String path) {
+        if (varFiles == null) varFiles = new HashSet<>();
+
+        if (path != null && path.startsWith("/")) {
+            path = path.trim().substring(1);
+        }
+
+        String file = String.format("%s/%s", repo.getIdentifier(), path);
+
+        this.varFiles.add(file);
+
+        return this;
+    }
+
+    public SetPipeline addVar(String key, String value) {
+        if (vars == null) vars = new JsonObject();
+
+        vars.addProperty(key, value);
+
+        return this;
+    }
+
+    public SetPipeline addVar(String key, JsonObject value) {
+        if (vars == null) vars = new JsonObject();
+
+        vars.add(key, value);
+
+        return this;
+    }
+
+    public SetPipeline addInstanceVar(String key, String value) {
+        if (instanceVars == null) instanceVars = new JsonObject();
+
+        instanceVars.addProperty(key, value);
+
+        return this;
+    }
+
+    public SetPipeline addInstanceVar(String key, JsonObject value) {
+        if (instanceVars == null) instanceVars = new JsonObject();
+
+        instanceVars.add(key, value);
 
         return this;
     }
