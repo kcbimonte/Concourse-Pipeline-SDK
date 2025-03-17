658af0ad8e6fdbe936234463153be56b570845c2
Kevin Bimonte <kbimonte@gmail.com>
2025-03-16 20:05:20 -0400

    test: Update setPipeline full pipeline test
    Missing inputs / outputs and additional resource config

--- src/test/java/org/concourseci/sdk/PipelineTest.java
+++ src/test/java/org/concourseci/sdk/PipelineTest.java
@@ -8,7 +8,6 @@ import org.concourseci.bundled.registry.RegistryImageConfig;
 import org.concourseci.bundled.registry.RegistryImageResourceType;
 import org.concourseci.sdk.resource.AnonymousResource;
 import org.concourseci.sdk.resource.Get;
-import org.concourseci.sdk.resource.Resource;
 import org.concourseci.sdk.step.SetPipeline;
 import org.concourseci.sdk.step.task.Command;
 import org.concourseci.sdk.step.task.Platform;
@@ -131,10 +130,58 @@ class PipelineTest {
 
         setExamples.addStep(blockedGet);
 
+        setExamples.addStep(SetPipeline.create("job", blockedGet, "pipelines/hello-world.yml"));
+        setExamples.addStep(SetPipeline.create("separate-task-config", blockedGet, "pipelines/separate-task-config.yml"));
+        setExamples.addStep(SetPipeline.create("serial-job", blockedGet, "pipelines/serial-job.yml"));
+
+        SetPipeline setPipelineVars = SetPipeline.create("pipeline-vars", blockedGet, "pipelines/pipeline-vars.yml");
+        setPipelineVars.addVar("first", "initial").addVar("number", "9000").addVar("hello", "HAL");
+        setExamples.addStep(setPipelineVars);
+
+        SetPipeline setPipelineVarsFile = SetPipeline.create("pipeline-vars-file", blockedGet, "pipelines/pipeline-vars.yml");
+        setPipelineVarsFile.addVarFile(blockedGet, "pipelines/vars-file.yml");
+        setExamples.addStep(setPipelineVarsFile);
+
+        SetPipeline instanceVarsOne = SetPipeline.create("instance-groups", blockedGet, "pipelines/pipeline-vars.yml");
+        SetPipeline instanceVarsTwo = SetPipeline.create("instance-groups", blockedGet, "pipelines/pipeline-vars.yml");
+        instanceVarsOne.addInstanceVar("first", "initial").addInstanceVar("number", "9000").addInstanceVar("hello", "HAL");
+        instanceVarsTwo.addInstanceVar("first", "second").addInstanceVar("number", "3000").addInstanceVar("hello", "WALLY-E");
+        setExamples.addStep(instanceVarsOne);
+        setExamples.addStep(instanceVarsTwo);
+
+        setExamples.addStep(SetPipeline.create("task-passing-artifact", blockedGet, "pipelines/task-passing-artifact.yml"));
+        setExamples.addStep(SetPipeline.create("time-triggered", blockedGet, "pipelines/time-triggered.yml"));
+        setExamples.addStep(SetPipeline.create("git-triggered", blockedGet, "pipelines/git-triggered.yml"));
+        setExamples.addStep(SetPipeline.create("manual-trigger", blockedGet, "pipelines/manually-triggered.yml"));
+        setExamples.addStep(SetPipeline.create("hooks", blockedGet, "pipelines/job-and-task-hooks.yml"));
+        setExamples.addStep(SetPipeline.create("golang-lib", blockedGet, "pipelines/golang-lib.yml"));
+        setExamples.addStep(SetPipeline.create("rails", blockedGet, "pipelines/rails-app-testing.yml"));
+        setExamples.addStep(SetPipeline.create("nodejs", blockedGet, "pipelines/nodejs-app-testing.yml"));
+        setExamples.addStep(SetPipeline.create("php", blockedGet, "pipelines/php-larvel-app-testing.yml"));
+        setExamples.addStep(SetPipeline.create("java", blockedGet, "pipelines/java.yml"));
+
         // Set Rendered Pipelines
         Job setRendered = new Job("set-rendered-pipelines").markPublic();
 
         setRendered.addStep(blockedGet);
+
+        AnonymousResource carvelytt = new AnonymousResource(RegistryImageResourceType.getInstance(), RegistryImageConfig.create("taylorsilva/carvel-ytt"));
+
+        String yttGen = """
+                ytt -f ./concourse-examples/pipelines/templates/simple > hello-world-rendered.yml
+                ytt -f ./concourse-examples/pipelines/templates/multiple-files > multi-files-rendered.yml
+                mv *.yml ./pipeline/
+                """;
+
+        Command renderCommand = Command.createCommand("sh").addArg("-cx").addArg(yttGen);
+
+        TaskConfig renderConfig = TaskConfig.create(Platform.LINUX, carvelytt, renderCommand);
+        Task renderTask = new Task("render-pipelines", renderConfig);
+
+        setRendered.addStep(renderTask);
+
+        setRendered.addStep(SetPipeline.create("hello-world-rendered","pipeline/hello-world-rendered.yml"));
+        setRendered.addStep(SetPipeline.create("multi-files-rendered","pipeline/multi-files-rendered.yml"));
 
         pipeline.addJob(setSelf).addJob(setExamples).addJob(setRendered);
 
