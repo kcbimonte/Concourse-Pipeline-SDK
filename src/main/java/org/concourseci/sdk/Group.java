c625d8e879b1c0eebb668c92f777e431adfbfd76
Kevin Bimonte <kbimonte@gmail.com>
2025-03-22 14:51:44 -0400

    feat: Add properties for Group

--- src/main/java/org/concourseci/sdk/Group.java
+++ src/main/java/org/concourseci/sdk/Group.java
@@ -1,4 +1,31 @@
 package org.concourseci.sdk;
 
+import org.concourseci.sdk.job.Job;
+import org.concourseci.sdk.util.Validator;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Group {
+    private final String name;
+
+    private final Set<String> jobs = new HashSet<>();
+
+    public Group(String name) {
+        Validator.validateIdentifier(name);
+
+        this.name = name;
+    }
+
+    public Group addJob(Job job) {
+        this.jobs.add(job.getName());
+
+        return this;
+    }
+
+    public Group addJobFilter(String jobFilter) {
+        this.jobs.add(jobFilter);
+
+        return this;
+    }
 }
