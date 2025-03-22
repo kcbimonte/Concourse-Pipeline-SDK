bee467d3ab5c48519e0f463d73c185778fafb6ae
Kevin Bimonte <kbimonte@gmail.com>
2025-03-22 14:55:01 -0400

    feat: Add Get Config (params) to Get

--- src/main/java/org/concourseci/sdk/resource/get/Get.java
+++ src/main/java/org/concourseci/sdk/resource/get/Get.java
@@ -20,6 +20,8 @@ public abstract class Get implements IStep {
 
     private Boolean trigger = false;
 
+    private IGetConfig params;
+
     @SerializedName("passed")
     private final List<String> passedJobs = new ArrayList<>();
 
---
+++ src/main/java/org/concourseci/sdk/resource/get/IGetConfig.java
@@ -0,0 +1,4 @@
+package org.concourseci.sdk.resource.get;
+
+public interface IGetConfig {
+}
