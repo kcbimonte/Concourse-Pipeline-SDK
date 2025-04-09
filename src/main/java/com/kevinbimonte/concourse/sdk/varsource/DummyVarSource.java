138838755918c68c67aec6714b9a0dee688fa606
Kevin Bimonte <kbimonte@gmail.com>
2025-04-09 17:46:00 -0400

    test: Test DummyVarSource

--- src/main/java/com/kevinbimonte/concourse/sdk/varsource/DummyVarSource.java
+++
@@ -1,7 +0,0 @@
-package com.kevinbimonte.concourse.sdk.varsource;
-
-public class DummyVarSource extends AbstractVarSource {
-    private DummyVarSource(String name, IVarSourceConfig config) {
-        super(name, VarSourceType.DUMMY, config);
-    }
-}
