26e1bed95c618f8c5d89b7c0323208ff09c934ec
Kevin Bimonte <kbimonte@gmail.com>
2025-04-10 06:33:53 -0400

    fix: Add serialized name
    NOTE: Should create an enum serializer in the future to auto do this

--- src/main/java/com/kevinbimonte/concourse/sdk/varsource/VarSourceType.java
+++ src/main/java/com/kevinbimonte/concourse/sdk/varsource/VarSourceType.java
@@ -1,7 +1,12 @@
 package com.kevinbimonte.concourse.sdk.varsource;
 
+import com.google.gson.annotations.SerializedName;
+
 public enum VarSourceType {
+    @SerializedName("vault")
     VAULT,
+    @SerializedName("ssm")
     SSM,
+    @SerializedName("dummy")
     DUMMY;
 }
