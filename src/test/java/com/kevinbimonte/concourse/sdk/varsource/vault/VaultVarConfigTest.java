307997cb17b9c3699015592e63d39b4694847966
Kevin Bimonte <kbimonte@gmail.com>
2025-04-13 15:01:58 -0400

    test: name test file correctly

--- src/test/java/com/kevinbimonte/concourse/sdk/varsource/vault/VaultVarConfigTest.java
+++ src/test/java/com/kevinbimonte/concourse/sdk/varsource/vault/VaultVarConfigTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class VaultVarSourceTest {
+class VaultVarConfigTest {
 
     @Test
     void setCaCert() {
