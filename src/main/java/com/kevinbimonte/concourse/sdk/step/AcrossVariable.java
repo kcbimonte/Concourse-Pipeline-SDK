a1842a608efef6dc44b5c2b8566617361f875339
Kevin Bimonte <kbimonte@gmail.com>
2025-04-27 11:50:25 -0400

    feat: Add basics of AcrossVariable

--- src/main/java/com/kevinbimonte/concourse/sdk/step/AcrossVariable.java
+++ src/main/java/com/kevinbimonte/concourse/sdk/step/AcrossVariable.java
@@ -1,4 +1,32 @@
 package com.kevinbimonte.concourse.sdk.step;
 
+import com.google.gson.JsonPrimitive;
+import com.google.gson.annotations.SerializedName;
+import com.kevinbimonte.concourse.sdk.util.Validator;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class AcrossVariable {
+    @SerializedName("var")
+    private final String name;
+
+    private final Set<Object> values = new HashSet<>();
+
+    @SerializedName("max_in_flight")
+    private JsonPrimitive maxInFlight;
+
+    private AcrossVariable(String name) {
+        Validator.validateIdentifier(name);
+
+        this.name = name;
+    }
+
+    public static AcrossVariable create(String name) {
+        return new AcrossVariable(name);
+    }
+
+    public String getVariable() {
+        return String.format("((.:%s))", this.name);
+    }
 }
