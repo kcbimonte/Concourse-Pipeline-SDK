dc011183b6eafd563f72fa6e07f929a8e6b326fe
Kevin Bimonte <kbimonte@gmail.com>
2025-04-28 06:38:34 -0400

    feat: Add final property for MockGetConfig

--- src/main/java/com/kevinbimonte/concourse/bundled/mock/get/MockGetConfig.java
+++ src/main/java/com/kevinbimonte/concourse/bundled/mock/get/MockGetConfig.java
@@ -5,15 +5,16 @@ import com.kevinbimonte.concourse.bundled.mock.MockConfig;
 import com.kevinbimonte.concourse.sdk.resource.get.IGetConfig;
 import lombok.Getter;
 
+import java.util.HashMap;
+
 @Getter
 public class MockGetConfig implements IGetConfig {
 
     @SerializedName("mirror_self_via_params")
     private Boolean mirrorSelfViaParams;
 
-//    @SerializedName("create_files_via_params")
-//    private Object createFilesViaParams;
-
+    @SerializedName("create_files_via_params")
+    private HashMap<String, String> createFiles;
 
     private MockGetConfig() {
     }
@@ -31,6 +32,27 @@ public class MockGetConfig implements IGetConfig {
      */
     public MockGetConfig mirrorSelf() {
         this.mirrorSelfViaParams = true;
+
+        return this;
+    }
+
+    /**
+     * Creates a new file to be consumed by the mock resource.
+     *
+     * @param fileName The name of the file (e.g. file1.yml)
+     * @param content  The content of the file. Should be a multi-line string.
+     * @return self
+     */
+    public MockGetConfig createNewFile(String fileName, String content) {
+        if (this.createFiles == null) {
+            this.createFiles = new HashMap<>();
+        }
+
+        if (this.createFiles.containsKey(fileName)) {
+            throw new IllegalArgumentException("Filename already exists in the map");
+        }
+
+        this.createFiles.put(fileName, content);
 
         return this;
     }
