c5f868b51dbece66ef5bd8179855b9f9ddf1dd0c
Kevin Bimonte <kbimonte@gmail.com>
2025-05-01 06:18:22 -0400

    feat: Finish Put structure to expose all properties for MockPut

--- src/main/java/com/kevinbimonte/concourse/sdk/resource/put/Put.java
+++ src/main/java/com/kevinbimonte/concourse/sdk/resource/put/Put.java
@@ -1,12 +1,22 @@
 package com.kevinbimonte.concourse.sdk.resource.put;
 
+import com.google.gson.*;
+import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
 import com.kevinbimonte.concourse.sdk.resource.Resource;
+import com.kevinbimonte.concourse.sdk.resource.get.Get;
 import com.kevinbimonte.concourse.sdk.resource.get.IGetConfig;
 import com.kevinbimonte.concourse.sdk.step.AbstractStep;
 import com.kevinbimonte.concourse.sdk.step.IStep;
+import com.kevinbimonte.concourse.sdk.step.task.config.Output;
 import com.kevinbimonte.concourse.sdk.util.Validator;
+import lombok.Getter;
 
+import java.lang.reflect.Type;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
 public abstract class Put extends AbstractStep<Put> implements IStep {
 
     @SerializedName("put")
@@ -15,6 +25,9 @@ public abstract class Put extends AbstractStep<Put> implements IStep {
     private String resource;
 
     private IPutConfig params;
+
+    @JsonAdapter(InputsSerializer.class)
+    private Set<String> inputs;
 
     @SerializedName("get_params")
     private IGetConfig getParams;
@@ -33,7 +46,75 @@ public abstract class Put extends AbstractStep<Put> implements IStep {
         this.resource = resource.getName();
     }
 
+    public Put setAllInputs() {
+        if (this.inputs == null) {
+            this.inputs = new HashSet<>();
+        }
+
+        if (!this.inputs.isEmpty()) {
+            this.inputs.clear();
+        }
+
+        this.inputs.add("all");
+
+        return this;
+    }
+
+    public Put setDetectInputs() {
+        if (this.inputs == null) {
+            this.inputs = new HashSet<>();
+        }
+
+        if (!this.inputs.isEmpty()) {
+            this.inputs.clear();
+        }
+
+        this.inputs.add("detect");
+
+        return this;
+    }
+
+    public Put addInput(Output output) {
+        if (this.inputs == null) {
+            this.inputs = new HashSet<>();
+        }
+
+        this.inputs.add(output.getName());
+
+        return this;
+    }
+
+    public Put addInput(Get get) {
+        if (this.inputs == null) {
+            this.inputs = new HashSet<>();
+        }
+
+        this.inputs.add(get.getIdentifier());
+
+        return this;
+    }
+
+    public Put setParams(IPutConfig config) {
+        this.params = config;
+
+        return this;
+    }
+
+    public Put setGetParams(IGetConfig config) {
+        if (this.noGet != null && this.noGet) {
+            throw new IllegalArgumentException("Cannot set get params when get is disabled");
+        }
+
+        this.getParams = config;
+
+        return this;
+    }
+
     public Put disableGet() {
+        if (this.getParams != null) {
+            throw new IllegalArgumentException("Cannot disable get when params are set");
+        }
+
         this.noGet = true;
 
         return this;
@@ -42,5 +123,30 @@ public abstract class Put extends AbstractStep<Put> implements IStep {
     @Override
     protected Put getSelf() {
         return this;
+    }
+
+    private static class InputsSerializer implements JsonSerializer<Set<String>> {
+
+        @Override
+        public JsonElement serialize(Set<String> src, Type typeOfSrc, JsonSerializationContext context) {
+
+            if (src.size() == 1 && src.contains("all")) {
+                return new JsonPrimitive("all");
+            }
+
+            if (src.size() == 1 && src.contains("detect")) {
+                return new JsonPrimitive("detect");
+            }
+
+            if (!src.isEmpty()) {
+                JsonArray arr = new JsonArray();
+
+                src.stream().map(JsonPrimitive::new).forEach(arr::add);
+
+                return arr;
+            }
+
+            return null;
+        }
     }
 }
