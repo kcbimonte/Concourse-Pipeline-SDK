57df57281c4fd2dd4d1aede6f86559a088cc647e
Kevin Bimonte <kbimonte@gmail.com>
2025-05-01 06:26:42 -0400

    test: Test MockResource
    Note: Remaining is Get and Put Configs

--- src/test/java/com/kevinbimonte/concourse/bundled/mock/MockResourceTest.java
+++ src/test/java/com/kevinbimonte/concourse/bundled/mock/MockResourceTest.java
@@ -4,8 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.kevinbimonte.concourse.bundled.mock.get.MockGet;
+import com.kevinbimonte.concourse.bundled.mock.put.MockPut;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,6 +33,90 @@ class MockResourceTest {
         // Assert
         assertNotNull(resource);
         assertEquals("mock", resource.getName());
+    }
+
+    @Test
+    void createStandardMockGet() {
+        // Arrange
+        MockConfig config = MockConfig.create();
+        MockResource resource = MockResource.createResource("mock", config);
+
+        // Act
+        MockGet get = resource.createGetDefinition();
+
+
+        // Assert
+        assertNotNull(get);
+        assertEquals("mock", get.getIdentifier());
+    }
+
+    @Test
+    void changeMockGetIdentifier() {
+        // Arrange
+        MockConfig config = MockConfig.create();
+        MockResource resource = MockResource.createResource("mock", config);
+
+        // Act
+        MockGet get = resource.createGetDefinition("identifier");
+
+
+        // Assert
+        assertNotNull(get);
+        assertEquals("identifier", get.getIdentifier());
+        assertEquals("mock", get.getResource());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123_get", "((my_var))-get", "MY_GET"})
+    void setInvalidMockGetIdentifier(String identifier) {
+        // Arrange
+        MockConfig config = MockConfig.create();
+        MockResource resource = MockResource.createResource("mock", config);
+
+        // Assert
+        assertThrows(IllegalArgumentException.class, () -> resource.createGetDefinition(identifier));
+    }
+
+    @Test
+    void createStandardMockPut() {
+        // Arrange
+        MockConfig config = MockConfig.create();
+        MockResource resource = MockResource.createResource("mock", config);
+
+        // Act
+        MockPut put = resource.createPutDefinition();
+
+
+        // Assert
+        assertNotNull(put);
+        assertEquals("mock", put.getIdentifier());
+    }
+
+    @Test
+    void changeMockPutIdentifier() {
+        // Arrange
+        MockConfig config = MockConfig.create();
+        MockResource resource = MockResource.createResource("mock", config);
+
+        // Act
+        MockPut put = resource.createPutDefinition("identifier");
+
+
+        // Assert
+        assertNotNull(put);
+        assertEquals("identifier", put.getIdentifier());
+        assertEquals("mock", put.getResource());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123_get", "((my_var))-get", "MY_GET"})
+    void setInvalidMockPutIdentifier(String identifier) {
+        // Arrange
+        MockConfig config = MockConfig.create();
+        MockResource resource = MockResource.createResource("mock", config);
+
+        // Assert
+        assertThrows(IllegalArgumentException.class, () -> resource.createPutDefinition(identifier));
     }
 
 
