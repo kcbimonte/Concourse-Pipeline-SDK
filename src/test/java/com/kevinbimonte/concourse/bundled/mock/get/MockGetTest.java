344d72e93acca2ff0d60a63f21ca64ee33fa393f
Kevin Bimonte <kbimonte@gmail.com>
2025-05-01 06:29:44 -0400

    test: Create empty test files for MockGet and MockPut to test configs and generation

---
+++ src/test/java/com/kevinbimonte/concourse/bundled/mock/get/MockGetTest.java
@@ -0,0 +1,7 @@
+package com.kevinbimonte.concourse.bundled.mock.get;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MockGetTest {
+
+}
---
+++ src/test/java/com/kevinbimonte/concourse/bundled/mock/put/MockPutTest.java
@@ -0,0 +1,7 @@
+package com.kevinbimonte.concourse.bundled.mock.put;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MockPutTest {
+
+}
