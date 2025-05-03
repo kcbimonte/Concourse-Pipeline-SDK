4fb76f1af9908925ac6423c65349970ea4d22785
Kevin Bimonte <kbimonte@gmail.com>
2025-05-03 09:03:57 -0400

    feat: Potential Implementation of AcrossVariable

--- src/main/java/com/kevinbimonte/concourse/sdk/step/AcrossVariable.java
+++ src/main/java/com/kevinbimonte/concourse/sdk/step/AcrossVariable.java
@@ -1,5 +1,6 @@
 package com.kevinbimonte.concourse.sdk.step;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.annotations.SerializedName;
 import com.kevinbimonte.concourse.sdk.util.Validator;
@@ -11,7 +12,7 @@ public class AcrossVariable {
     @SerializedName("var")
     private final String name;
 
-    private final Set<Object> values = new HashSet<>();
+    private final Set<JsonElement> values = new HashSet<>();
 
     @SerializedName("max_in_flight")
     private JsonPrimitive maxInFlight;
@@ -28,5 +29,35 @@ public class AcrossVariable {
 
     public String getVariable() {
         return String.format("((.:%s))", this.name);
+    }
+
+    public void setMaxInFlight(Integer maxInFlight) {
+        if (maxInFlight < 1) {
+            throw new IllegalArgumentException("Max in Flight cannot be less than 1");
+        }
+
+        this.maxInFlight = new JsonPrimitive(maxInFlight);
+    }
+
+    public void runAll() {
+        this.maxInFlight = new JsonPrimitive("all");
+    }
+
+    public AcrossVariable addValue(Integer value) {
+        this.values.add(new JsonPrimitive(value));
+
+        return this;
+    }
+
+    public AcrossVariable addValue(String value) {
+        this.values.add(new JsonPrimitive(value));
+
+        return this;
+    }
+
+    public AcrossVariable addValue(Boolean value) {
+        this.values.add(new JsonPrimitive(value));
+
+        return this;
     }
 }
