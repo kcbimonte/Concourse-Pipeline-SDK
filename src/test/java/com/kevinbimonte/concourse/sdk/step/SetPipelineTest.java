8857f665c00531460102bb7f71ecc29ca6e45dee
Kevin Bimonte <kbimonte@gmail.com>
2025-05-03 11:24:28 -0400

    test: Across Set Pipeline test

--- src/test/java/com/kevinbimonte/concourse/sdk/step/SetPipelineTest.java
+++ src/test/java/com/kevinbimonte/concourse/sdk/step/SetPipelineTest.java
@@ -2,8 +2,12 @@ package com.kevinbimonte.concourse.sdk.step;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.kevinbimonte.concourse.bundled.git.GitResource;
+import com.kevinbimonte.concourse.bundled.git.GitResourceConfig;
+import com.kevinbimonte.concourse.bundled.git.get.GitGet;
 import com.kevinbimonte.concourse.sdk.Pipeline;
 import com.kevinbimonte.concourse.sdk.TestUtils;
+import com.kevinbimonte.concourse.sdk.job.Job;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,8 +18,21 @@ class SetPipelineTest {
     void setPipelineAcross() {
         // Arrange
         Pipeline pipeline = new Pipeline();
+        GitResource ci = GitResource.createResource("ci", GitResourceConfig.create("https://github.com/concourse/examples.git"));
+        pipeline.addResource(ci);
+
+        Job job = new Job("job");
+        GitGet getDefinition = ci.createGetDefinition();
+        job.addStep(getDefinition);
+
+        AcrossVariable variable = AcrossVariable.create("pipeline").addValue("hello-world").addValue("time-triggered");
+
+        String pipelineFile = String.format("pipelines/%s.yml", variable.getVariable());
+        SetPipeline setPipeline = SetPipeline.createAcrossPipeline(variable.getVariable(), getDefinition, pipelineFile, variable);
 
         // Act
+        job.addStep(setPipeline);
+        pipeline.addJob(job);
 
         // Assert
         JsonElement generated = JsonParser.parseString(pipeline.render());
