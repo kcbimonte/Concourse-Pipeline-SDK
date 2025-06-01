bec5dbb9103bb6965407b77fc4e47d5ccf62199e
Kevin Bimonte <kbimonte@gmail.com>
2025-06-01 15:47:56 -0400

    test: move around test for ResourceType

--- src/test/java/com/kevinbimonte/concourse/bundled/registry/RegistryImageResourceTest.java
+++ src/test/java/com/kevinbimonte/concourse/bundled/registry/RegistryImageResourceTest.java
@@ -1,38 +1,7 @@
 package com.kevinbimonte.concourse.bundled.registry;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.kevinbimonte.concourse.sdk.Pipeline;
-import com.kevinbimonte.concourse.sdk.TestUtils;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class RegistryImageResourceTest {
 
-    @Test
-    void separateTaskConfig() {
-        // Arrange
-        Pipeline pipeline = new Pipeline();
-
-        // TODO: Not sure I like how this works
-        RegistryImageResourceType type = RegistryImageResourceType
-                .create(RegistryImageConfig.create("concourse/registry-image-resource"))
-                .setDefaults(RegistryImageConfig.create(null)
-                        .setRegistryMirror(RegistryMirrorConfig.create("https://registry.mirror.example.com"))
-                );
-
-        pipeline.addResourceType(type);
-
-        RegistryImageResource resource = RegistryImageResource.create("mirrored-image", RegistryImageConfig.create("busybox"));
-        pipeline.addResource(resource);
-
-        // Act
-        JsonElement generated = JsonParser.parseString(pipeline.render());
-
-        // Assert
-        JsonElement expected = TestUtils.loadFromAssets("resource-type/default.json");
-
-        assertEquals(expected, generated);
-    }
 }
